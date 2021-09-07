5b8ba967d324692f5c4bf0cdb9ccdf4af26ab708
Reto Brunner <reto@labrat.space>
2021-09-07 09:16:47 +0200

    add null-ls, simplify formatting

--- lua/rpb/plugins.lua
+++ lua/rpb/plugins.lua
@@ -1,141 +1,161 @@
-require('packer').startup(function(use)
-  -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
+require("packer").startup(function(use)
+	-- Packer can manage itself
+	use("wbthomason/packer.nvim")
 
-    --use "wbthomason/lsp-status.nvim"
-  use {"brunnre8/gruvbox.nvim", requires = {"rktjmp/lush.nvim"}}
+	--use "wbthomason/lsp-status.nvim"
+	use({ "brunnre8/gruvbox.nvim", requires = { "rktjmp/lush.nvim" } })
 
+	use("tpope/vim-repeat")
+	use("tpope/vim-surround")
+	use("tpope/vim-commentary")
+	use("tpope/vim-fugitive")
 
-  use 'tpope/vim-repeat'
-  use 'tpope/vim-surround'
-  use 'tpope/vim-commentary'
-  use 'tpope/vim-fugitive'
+	use({
+		"nvim-telescope/telescope.nvim",
+		requires = {
+			{ "nvim-lua/popup.nvim" },
+			{ "nvim-lua/plenary.nvim" },
+		},
+	})
 
-  use { 'nvim-telescope/telescope.nvim',
-    requires = {
-      { 'nvim-lua/popup.nvim' },
-      { 'nvim-lua/plenary.nvim' }
-    }
-  }
+	use("scrooloose/nerdtree")
 
-  use 'scrooloose/nerdtree'
+	-- Highlight, edit, and navigate code using a fast incremental parsing library
+	use("nvim-treesitter/nvim-treesitter")
+	use("nvim-treesitter/playground")
+	use("lewis6991/spellsitter.nvim") -- treesitter messes up the spell detection of nvim
+	-- Additional textobjects for treesitter
+	use("nvim-treesitter/nvim-treesitter-textobjects")
+	use("neovim/nvim-lspconfig") -- Collection of configurations for built-in LSP client
+	use("ray-x/lsp_signature.nvim")
+	use("hrsh7th/nvim-compe") -- Autocompletion plugin
+	-- use 'L3MON4D3/LuaSnip' -- Snippets plugin
+	use({
+		"lewis6991/gitsigns.nvim",
+		requires = {
+			"nvim-lua/plenary.nvim",
+		},
 
-  -- Highlight, edit, and navigate code using a fast incremental parsing library
-  use 'nvim-treesitter/nvim-treesitter'
-  use 'nvim-treesitter/playground'
-  use 'lewis6991/spellsitter.nvim' -- treesitter messes up the spell detection of nvim
-  -- Additional textobjects for treesitter
-  use 'nvim-treesitter/nvim-treesitter-textobjects'
-  use 'neovim/nvim-lspconfig' -- Collection of configurations for built-in LSP client
-  use 'ray-x/lsp_signature.nvim'
-  use 'hrsh7th/nvim-compe' -- Autocompletion plugin
-  -- use 'L3MON4D3/LuaSnip' -- Snippets plugin
-  use {
-    'lewis6991/gitsigns.nvim',
-    requires = {
-      'nvim-lua/plenary.nvim'
-    }
-  }
-
+		use({
+			"jose-elias-alvarez/null-ls.nvim",
+			requires = {
+				"nvim-lua/plenary.nvim",
+				"neovim/nvim-lspconfig",
+			},
+		}),
+	})
 end)
 
-require('gitsigns').setup({
-  signs = {
-    add          = {hl = 'GitSignsAdd'   , text = '│', numhl='GitSignsAddNr'   , linehl='GitSignsAddLn'},
-    change       = {hl = 'GitSignsChange', text = '│', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn'},
-    delete       = {hl = 'GitSignsDelete', text = '|', numhl='GitSignsDeleteNr', linehl='GitSignsDeleteLn'},
-    topdelete    = {hl = 'GitSignsDelete', text = '|', numhl='GitSignsDeleteNr', linehl='GitSignsDeleteLn'},
-    changedelete = {hl = 'GitSignsChange', text = '|', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn'},
-  },
-  keymaps={
-    ['n ]c'] = { expr = true, "&diff ? ']c' : '<cmd>lua require\"gitsigns\".next_hunk()<CR>'"},
-    ['n [c'] = { expr = true, "&diff ? '[c' : '<cmd>lua require\"gitsigns\".prev_hunk()<CR>'"},
-  },
+require("gitsigns").setup({
+	signs = {
+		add = { hl = "GitSignsAdd", text = "│", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
+		change = { hl = "GitSignsChange", text = "│", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
+		delete = { hl = "GitSignsDelete", text = "|", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
+		topdelete = { hl = "GitSignsDelete", text = "|", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
+		changedelete = { hl = "GitSignsChange", text = "|", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
+	},
+	keymaps = {
+		["n ]c"] = { expr = true, "&diff ? ']c' : '<cmd>lua require\"gitsigns\".next_hunk()<CR>'" },
+		["n [c"] = { expr = true, "&diff ? '[c' : '<cmd>lua require\"gitsigns\".prev_hunk()<CR>'" },
+	},
 })
 
-require('nvim-treesitter.configs').setup({
-  ensure_installed = 'maintained',
-  highlight = {enable=true},
-  incremental_selection = {
-    enable=true,
-    keymaps = {
-      init_selection = "gnn",
-      node_incremental = "grn",
-      node_decremental = "grm",
-      scope_incremental = "grc",
-    },
-  },
+require("nvim-treesitter.configs").setup({
+	ensure_installed = "maintained",
+	highlight = { enable = true },
+	incremental_selection = {
+		enable = true,
+		keymaps = {
+			init_selection = "gnn",
+			node_incremental = "grn",
+			node_decremental = "grm",
+			scope_incremental = "grc",
+		},
+	},
 })
 
-require('spellsitter').setup({captures = {'comment', 'string'}})
+require("spellsitter").setup({ captures = { "comment", "string" } })
 
-require('compe').setup({
-  enabled = true,
-  source = {
-    path = true,
-    nvim_lsp = true,
-    luasnip = true,
-    buffer = true,
-    calc = false,
-    nvim_lua = false,
-    vsnip = false,
-    ultisnips = false,
-  },
+require("compe").setup({
+	enabled = true,
+	source = {
+		path = true,
+		nvim_lsp = true,
+		luasnip = true,
+		buffer = true,
+		calc = false,
+		nvim_lua = false,
+		vsnip = false,
+		ultisnips = false,
+	},
 })
 
 -- LSP settings
 
 function _G.goimports(timeout_ms)
-    local context = { only = { "source.organizeImports" } }
-    vim.validate { context = { context, "t", true } }
+	local context = { only = { "source.organizeImports" } }
+	vim.validate({ context = { context, "t", true } })
 
-    local params = vim.lsp.util.make_range_params()
-    params.context = context
+	local params = vim.lsp.util.make_range_params()
+	params.context = context
 
-    -- See the implementation of the textDocument/codeAction callback
-    -- (lua/vim/lsp/handler.lua) for how to do this properly.
-    local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, timeout_ms)
-    if not result or next(result) == nil then return end
-    local actions = result[1].result
-    if not actions then return end
-    local action = actions[1]
+	-- See the implementation of the textDocument/codeAction callback
+	-- (lua/vim/lsp/handler.lua) for how to do this properly.
+	local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, timeout_ms)
+	if not result or next(result) == nil then
+		return
+	end
+	local actions = result[1].result
+	if not actions then
+		return
+	end
+	local action = actions[1]
 
-    -- textDocument/codeAction can return either Command[] or CodeAction[]. If it
-    -- is a CodeAction, it can have either an edit, a command or both. Edits
-    -- should be executed first.
-    if action.edit or type(action.command) == "table" then
-        if action.edit then
-            vim.lsp.util.apply_workspace_edit(action.edit)
-        end
-        if type(action.command) == "table" then
-            vim.lsp.buf.execute_command(action.command)
-        end
-    else
-        vim.lsp.buf.execute_command(action)
-    end
+	-- textDocument/codeAction can return either Command[] or CodeAction[]. If it
+	-- is a CodeAction, it can have either an edit, a command or both. Edits
+	-- should be executed first.
+	if action.edit or type(action.command) == "table" then
+		if action.edit then
+			vim.lsp.util.apply_workspace_edit(action.edit)
+		end
+		if type(action.command) == "table" then
+			vim.lsp.buf.execute_command(action.command)
+		end
+	else
+		vim.lsp.buf.execute_command(action)
+	end
 end
 
-local lspconfig = require('lspconfig')
-local on_attach = function(_, bufnr)
-    vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
-    local opts = { noremap = true, silent = true }
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>D', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>d', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>i', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+local lspconfig = require("lspconfig")
+local on_attach = function(client, bufnr)
+	vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
+	local opts = { noremap = true, silent = true }
+	vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>D", "<Cmd>lua vim.lsp.buf.declaration()<CR>", opts)
+	vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>d", "<Cmd>lua vim.lsp.buf.definition()<CR>", opts)
+	vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>i", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
+	vim.api.nvim_buf_set_keymap(bufnr, "n", "K", "<Cmd>lua vim.lsp.buf.hover()<CR>", opts)
+	vim.api.nvim_buf_set_keymap(bufnr, "n", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
 
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>lrn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>lr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>lca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>ldl', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>la', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
+	vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>lrn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
+	vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>lr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
+	vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>lca", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
+	vim.api.nvim_buf_set_keymap(bufnr, "n", "[d", "<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>", opts)
+	vim.api.nvim_buf_set_keymap(bufnr, "n", "]d", "<cmd>lua vim.lsp.diagnostic.goto_next()<CR>", opts)
+	vim.api.nvim_buf_set_keymap(
+		bufnr,
+		"n",
+		"<leader>ldl",
+		"<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>",
+		opts
+	)
+	vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>la", "<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>", opts)
 
-    vim.cmd [[ command! LspFormat execute 'lua vim.lsp.buf.formatting()' ]]
+	if client.resolved_capabilities.document_formatting then
+		vim.cmd("autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync(nil, 1000)")
+	end
+	vim.cmd([[ command! LspFormat execute 'lua vim.lsp.buf.formatting()' ]])
 
-	require('lsp_signature').on_attach({
+	require("lsp_signature").on_attach({
 		hint_prefix = "",
 		hint_enable = false,
 		floating_window = true,
@@ -147,67 +167,78 @@ local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities.textDocument.completion.completionItem.snippetSupport = true
 
 local function lsp_server(lsp, opts)
-    local options = {
-        on_attach = on_attach,
-        capabilities = capabilities,
-    }
-    if opts then options = vim.tbl_extend('force', options, opts) end
-    lspconfig[lsp].setup(options)
+	local options = {
+		on_attach = on_attach,
+		capabilities = capabilities,
+	}
+	if opts then
+		options = vim.tbl_extend("force", options, opts)
+	end
+	lspconfig[lsp].setup(options)
 end
 
 lsp_server("gopls", {
-    init_options = {
-    usePlaceholders = false,
-    completeUnimported = true,
-    hoverKind = "FullDocumentation",
-    buildFlags = {"-tags", "notmuch"},
-    analyses = {
-        composites = false,
-        fillreturns = true,
-        nonewvars = true,
-        unusedparams = true,
-        shadow = true
-        },
-    linksInHover = false,
-    staticcheck = true
-    }
+	init_options = {
+		usePlaceholders = false,
+		completeUnimported = true,
+		hoverKind = "FullDocumentation",
+		buildFlags = { "-tags", "notmuch" },
+		analyses = {
+			composites = false,
+			fillreturns = true,
+			nonewvars = true,
+			unusedparams = true,
+			shadow = true,
+		},
+		linksInHover = false,
+		staticcheck = true,
+	},
 })
 
 lsp_server("ccls")
 lsp_server("pylsp")
 lsp_server("bashls")
 
-local runtime_path = vim.split(package.path, ';')
+local runtime_path = vim.split(package.path, ";")
 table.insert(runtime_path, "lua/?.lua")
 table.insert(runtime_path, "lua/?/init.lua")
+-- table.insert(runtime_path,vim.fn.expand('$VIMRUNTIME/lua'))
+-- table.insert(runtime_path,vim.fn.expand('$VIMRUNTIME/lua/vim/lsp'))
 lsp_server("sumneko_lua", {
-    cmd = {"/usr/bin/lua-language-server"},
-    settings = {
-        Lua = {
-            runtime = {
-                version = 'LuaJIT', -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-                path = runtime_path, -- Setup your lua path
-            },
-            diagnostics = {
-                globals = {'vim'}, -- Get the language server to recognize the `vim` global
-            },
-            workspace = {
-                library = vim.api.nvim_get_runtime_file("", true), -- Make the server aware of Neovim runtime files
-            },
-            telemetry = {
-                enable = false,
-            },
-        },
-    },
-
+	cmd = { "/usr/bin/lua-language-server" },
+	settings = {
+		Lua = {
+			runtime = {
+				version = "LuaJIT", -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+				path = runtime_path, -- Setup your lua path
+			},
+			diagnostics = {
+				globals = { "vim" }, -- Get the language server to recognize the `vim` global
+			},
+			workspace = {
+				library = vim.api.nvim_get_runtime_file("", true), -- Make the server aware of Neovim runtime files
+			},
+			telemetry = {
+				enable = false,
+			},
+		},
+	},
 })
 
+local null_ls = require("null-ls")
+null_ls.config({
+	sources = {
+		null_ls.builtins.formatting.stylua,
+	},
+	debug = false,
+})
+
+lsp_server("null-ls")
+
 vim.cmd([[
-augroup lsp_formatters
+augroup formatters
     au!
-    au BufWritePre *.go lua vim.lsp.buf.formatting_sync(nil, 1000)
     au BufWritePre *.go lua goimports(1000)
-    au BufWritePre *.py lua vim.lsp.buf.formatting_sync(nil, 1000)
 augroup END
 ]])
 
