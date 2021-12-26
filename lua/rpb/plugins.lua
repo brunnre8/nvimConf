e835486f9ed3a007d8ee9573d4853bd76bfc86f0
Reto Brunner <reto@labrat.space>
2021-12-26 22:28:34 +0100

    Allow scrolling in preview

--- lua/rpb/plugins.lua
+++ lua/rpb/plugins.lua
@@ -120,7 +120,7 @@ cmp.setup({
 	},
 
 	mapping = {
-		["<C-d>"] = cmp.mapping.scroll_docs(-4),
+		["<C-b>"] = cmp.mapping.scroll_docs(-4),
 		["<C-f>"] = cmp.mapping.scroll_docs(4),
 		["<C-Space>"] = cmp.mapping.complete(),
 		["<C-g>"] = cmp.mapping.close(),
