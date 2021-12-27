a92847c7b1e18dcb612e100d8de621d9fe43d551
Reto Brunner <reto@labrat.space>
2021-12-27 09:16:42 +0100

    Globals: make module

--- lua/rpb/globals.lua
+++ lua/rpb/globals.lua
@@ -1,5 +1,14 @@
+local M = {}
 -- Pretty print thing to the console
-function P(thing)
+function M.P(thing)
 	print(vim.inspect(thing))
 	return thing
 end
+
+-- Replace a Termcode string to the internal representation
+-- Use this say for returning "C-t" or "<TAB>" for mappings
+function M.t(str)
+	return vim.api.nvim_replace_termcodes(str, true, true, true)
+end
+
+return M
