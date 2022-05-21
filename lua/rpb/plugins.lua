3bce30d3326ca44ebc1cdbc45ccae902e2dd4266
Reto Brunner <reto@labrat.space>
2022-05-21 14:25:35 +0200

    remove obsolete capability override
    This is already performed by the call to cmp_nvim_lsp

--- lua/rpb/plugins.lua
+++ lua/rpb/plugins.lua
@@ -256,7 +256,6 @@ end
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require("cmp_nvim_lsp").update_capabilities(capabilities)
-capabilities.textDocument.completion.completionItem.snippetSupport = true
 
 local function lsp_server(lsp, opts, on_attach_pre)
 	local on_attach_func = on_attach
