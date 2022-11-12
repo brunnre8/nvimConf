455fd6c5cbabf31d16a46a9ca0304ef802e230f3
Reto Brunner <reto@slightlybroken.com>
2022-11-12 14:56:58 +0100

    cmp_nvim_lsp: switch to non deprecated cap handling

--- lua/rpb/plugins.lua
+++ lua/rpb/plugins.lua
@@ -273,8 +273,7 @@ local on_attach = function(client, bufnr)
 	})
 end
 
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require("cmp_nvim_lsp").update_capabilities(capabilities)
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
 local function lsp_server(lsp, opts, on_attach_pre)
 	local on_attach_func = on_attach
