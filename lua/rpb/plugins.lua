ddfc92397e9e00b22aa33075f4124b9d0069a5ae
Reto Brunner <reto@slightlybroken.com>
2023-02-25 16:11:35 +0100

    fix typo in config

--- lua/rpb/plugins.lua
+++ lua/rpb/plugins.lua
@@ -244,7 +244,7 @@ snippy.setup({
 })
 
 require("nvim-autopairs").setup({
-	disable_in_marcro = true,
+	disable_in_macro = true,
 })
 -- local cmp_autopairs = require("nvim-autopairs.completion.cmp")
 -- cmp.event:on( 'confirm_done', cmp_autopairs.on_confirm_done())
