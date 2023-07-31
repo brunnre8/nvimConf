22d9a5c3e0758122e4f7380749af570f7ccd9f19
Reto Brunner <reto@slightlybroken.com>
2023-07-31 23:28:58 +0200

    go: run go imports synchronously
    Sadly, the easy way doesn't work as the imports action is run async
    meaning by the time it's done we already saved.
    
    So we need to do it the long winded way as there's no async param

--- lua/rpb/plugins.lua
+++ lua/rpb/plugins.lua
@@ -272,6 +272,19 @@ vim.api.nvim_create_autocmd("BufWritePre", {
 	group = lsp_au,
 	callback = function()
 		vim.lsp.buf.code_action({ context = { only = { "source.organizeImports" } }, apply = true })
+		local params = vim.lsp.util.make_range_params(nil, vim.lsp.util._get_offset_encoding())
+		params.context = { only = { "source.organizeImports" } }
+
+		local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, 3000)
+		for _, res in pairs(result or {}) do
+			for _, r in pairs(res.result or {}) do
+				if r.edit then
+					vim.lsp.util.apply_workspace_edit(r.edit, vim.lsp.util._get_offset_encoding())
+				else
+					vim.lsp.buf.execute_command(r.command)
+				end
+			end
+		end
 	end,
 })
 
