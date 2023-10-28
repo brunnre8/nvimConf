9bfbba88093b489ef5f2f7e1b368baebac0f06be
Reto Brunner <reto@slightlybroken.com>
2023-10-28 20:25:27 +0200

    remove pretty print func
    We have that in core

--- lua/rpb/globals.lua
+++ lua/rpb/globals.lua
@@ -1,10 +1,4 @@
 local M = {}
--- Pretty print thing to the console
-function M.P(thing)
-	print(vim.inspect(thing))
-	return thing
-end
-
 -- Replace a Termcode string to the internal representation
 -- Use this say for returning "C-t" or "<TAB>" for mappings
 function M.t(str)
