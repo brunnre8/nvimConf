efa0aac994b0e63812f42756f6bd3481c4788877
Reto Brunner <reto@slightlybroken.com>
2024-03-28 14:30:19 +0100

    go: remove doubled code_action from au command

--- lua/rpb/plugins.lua
+++ lua/rpb/plugins.lua
@@ -302,15 +302,14 @@ vim.api.nvim_create_autocmd("BufWritePre", {
 	group = lsp_au,
 	pattern = { "*.go" },
 	callback = function()
-		vim.lsp.buf.code_action({ context = { only = { "source.organizeImports" } }, apply = true })
-		local params = vim.lsp.util.make_range_params(nil, vim.lsp.util._get_offset_encoding())
+		local params = vim.lsp.util.make_range_params(nil, vim.lsp.util._get_offset_encoding(0))
 		params.context = { only = { "source.organizeImports" } }
 
 		local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, 3000)
 		for _, res in pairs(result or {}) do
 			for _, r in pairs(res.result or {}) do
 				if r.edit then
-					vim.lsp.util.apply_workspace_edit(r.edit, vim.lsp.util._get_offset_encoding())
+					vim.lsp.util.apply_workspace_edit(r.edit, vim.lsp.util._get_offset_encoding(0))
 				else
 					vim.lsp.buf.execute_command(r.command)
 				end
