9a769089762fedbc22e6a6e6da40a8770d499cfe
Reto Brunner <reto@slightlybroken.com>
2024-05-10 19:57:43 +0200

    vim.loop is renamed to vim.uv

--- lua/rpb/plugins.lua
+++ lua/rpb/plugins.lua
@@ -1,6 +1,6 @@
 -- auto fetch the package manager
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
+if not vim.uv.fs_stat(lazypath) then
 	vim.fn.system({
 		"git",
 		"clone",
